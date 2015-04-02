adc002bb8c626369e5e7ad4ce21ccf49c70b9d00
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-02 16:28:44 -0400

    Added a method to recognize the assortment of movements

--- rover.rb
+++ rover.rb
@@ -7,6 +7,22 @@ class Rover
 		@direction = init_direction
 	end
 
+	def userInput
+		puts "Enter an arrangment of directions for the rover"
+		input = gets.chomp
+		values = input.split("")
+
+		values.each do |value|
+			if value == "l"
+				turnLeft
+			elsif value == "r"
+				turnRight
+			elsif value == "m"
+				move_forward
+			end
+		end
+	end
+
 	def move_forward
 		if @direction == "N"
 			@y += 1
@@ -32,7 +48,15 @@ class Rover
 	end
 
 	def turnRight
-
+		if @direction == "N"
+			@direction = "E"
+		elsif @direction == "E"
+			@direction = "S"
+		elsif @direction == "S"
+			@direction = "W"
+		elsif @direction == "W"
+			@direction = "N"
+		end
 	end
 
 	def to_s
@@ -40,7 +64,10 @@ class Rover
 	end
 end
 
-my_rover = Rover.new(2, 2, "N")
+#Start of MAIN
+
+
+my_rover = Rover.new(1, 1, "N")
 puts my_rover
-my_rover.turnLeft
+my_rover.userInput
 puts my_rover
