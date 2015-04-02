d69dab1d10970f3d3c7109dc6a59d90463ee6fcd
Daniel Dvorkin <Dvorkin212@gmail.com>
2015-04-02 15:13:48 -0400

    Added some methods to the class Rover

--- rover.rb
+++ rover.rb
@@ -7,12 +7,28 @@ class Rover
 		@direction = init_direction
 	end
 
-	def move
-
+	def move_forward
+		if @direction == "N"
+			@y += 1
+		elsif @direction == "W"
+			@x -= 1
+		elsif @direction == "S"
+			@y -= 1
+		elsif @direction == "E"
+			@x += 1
+		end
 	end
 
 	def turnLeft
-
+		if @direction == "N"
+			@direction = "W"
+		elsif @direction == "W"
+			@direction = "S"
+		elsif @direction == "S"
+			@direction = "E"
+		elsif @direction == "E"
+			@direction = "N"
+		end
 	end
 
 	def turnRight
@@ -20,9 +36,11 @@ class Rover
 	end
 
 	def to_s
-		"I am at #{@x}, #{@y}, facing: #{@direction}"
+		"I am at x:#{@x}, y:#{@y}, facing: #{@direction}"
 	end
 end
 
 my_rover = Rover.new(2, 2, "N")
+puts my_rover
+my_rover.turnLeft
 puts my_rover
